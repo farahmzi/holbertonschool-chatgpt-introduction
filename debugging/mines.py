d5f4f64aeb51ab0713276b5ecc9f1725f5d61347
farahmzi <farahalmzini@gmail.com>
2025-07-19 01:33:14 +0000

    Add mines.py

---
+++ debugging/mines.py
@@ -0,0 +1,23 @@
+def has_won(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                if (y * self.width + x) not in self.mines and not self.revealed[y][x]:
+                    return False
+        return True
+
+    def play(self):
+        while True:
+            self.print_board()
+            try:
+                x = int(input("Enter x coordinate: "))
+                y = int(input("Enter y coordinate: "))
+                if not self.reveal(x, y):
+                    self.print_board(reveal=True)
+                    print("Game Over! You hit a mine.")
+                    break
+                elif self.has_won():
+                    self.print_board(reveal=True)
+                    print("Congratulations! You've won the game.")
+                    break
+            except ValueError:
+                print("Invalid input. Please enter numbers only.")
